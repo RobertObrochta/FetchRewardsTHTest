29a03a9f6961695b39297b631276d170a36d781f
Obrochta <ROBROCH1@depaul.edu>
2022-10-03 19:16:55 -0500

    Finished form control and CSS

--- exercise/src/components/Form.js
+++ exercise/src/components/Form.js
@@ -1,5 +1,5 @@
 import Completed from './Completed';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const Form = () => {
     const [visitor, setVisitor] = useState({
@@ -9,6 +9,9 @@ const Form = () => {
         occupation: '',
         state: ''
     });
+
+    const [formErrors, setFormErrors] = useState({}); // builds up the errors, but doeos not change the form style until submit is hit.
+    const initialRender = useRef(true);
     
     // fields to be fed into visitor object once submitted and complete
     const [fullName, setFullName] = useState('');
@@ -17,8 +20,9 @@ const Form = () => {
     const [occupation, setOccupation] = useState('');
     const [state, setState] = useState('');
 
-    const submitForm = () => { // POST will be here, upload whole state of visitor as a JSON object within the request body
-        console.log("Submitting form...");
+    const [formSubmitted, setFormSubmitted] = useState(false);
+
+    const submitForm = () => { // TODO: POST will be here, upload whole state of visitor as a JSON object within the request body
         setVisitor({
             name: fullName, 
             password: password,
@@ -26,21 +30,40 @@ const Form = () => {
             occupation: occupation,
             state: state
         });
+        console.log("Submitting form... with user info:", visitor);
     }
 
     const validationHandler = () => {
-        return checkForCompleteForm(visitor.name, visitor.password, visitor.email, visitor.occupation, visitor.state);
+        setFormSubmitted(true);
+        return checkForCompleteForm(fullName, password, email, occupation, state);
     }
 
     const checkForCompleteForm = (fullName, password, email, occupation, state) => {
-        if (fullName.length >= 1 && password.length >= 1 && email.length >= 1 && occupation.length >= 1 && state.length >= 1){
+        let errors = {}; // update formErrors with this value
+
+        if (formErrors === {}){
             return true;
         }
-        console.log("Form isn't complete"); // add some sort of red boxes to the forms that the user missed
+        else{
+            if (fullName === ''){errors["name"] = "Name cannot be empty."}
+            if (password === ''){errors["password"] = "Password cannot be empty."}
+            if (email === ''){errors["email"] = "Email cannot be empty."} // TODO: check for email format
+            if (occupation === ''){errors["occupation"] = "Please select an occupation."}
+            if (state === ''){errors["state"] = "Please select a state."}
+        setFormErrors(errors);
+        }
         return false;
     }
 
-    if (validationHandler === true){
+    useEffect(() => {
+        if (initialRender.current){
+            initialRender.current = false;
+            return;
+        }
+        checkForCompleteForm(fullName, password, email, occupation, state);
+    }, [fullName, password, email, occupation, state])
+
+    if (validationHandler === true){ // if no errors
         submitForm();
         return (
             <Completed/>
@@ -50,35 +73,38 @@ const Form = () => {
     return(
         <div id="Form">
             <form className="neat-form">
-                <label>
-                    Full Name:
-                    <input type="text" name="name" placeholder='Enter full name...' onChange={(e) => setFullName(e.target.value)}/>
+                <label>Full Name: 
+                    {formSubmitted && <p className = "form-error">{formErrors["name"]}</p>}
+                    <input type="text" name="name" placeholder='Enter full name...' style={formSubmitted && formErrors["name"] ? {borderColor: "red"} : null} 
+                    onChange={(e) => setFullName(e.target.value)} />
                 </label>
-                <label>
-                    Email Address
-                    <input type="text" name="email" placeholder='Enter email address...' onChange={(e) => setEmail(e.target.value)}/>
+                <label>Email Address 
+                    {formSubmitted && <p className = "form-error">{formErrors["email"]}</p>}
+                    <input type="email" name="email" placeholder='Enter email address...' style={formSubmitted && formErrors["email"] ? {borderColor: "red"} : null} 
+                    onChange={(e) => setEmail(e.target.value)}/>
                 </label>
-                <label> {/* Make this masked with an eye thing */}
-                    Password
-                    <input type="text" name="password" placeholder='Password...' onChange={(e) => setPassword(e.target.value)}/>
+                <label>Password 
+                    {formSubmitted && <p className = "form-error">{formErrors["password"]}</p>}
+                    <input type="password" name="password" placeholder='Password...' style={formSubmitted && formErrors["password"] ? {borderColor: "red"} : null} 
+                    onChange={(e) => setPassword(e.target.value)}/>
                 </label>
-                <label> {/* map function ==> option from the GET request */}
-                    Occupation
-                    <select name="password" placeholder='Select an occupation...' onChange={(e) => setOccupation(e.target.value)}>
-
+                <label> {/* TODO map function ==> option from the GET request */}Occupation 
+                {formSubmitted && <p className = "form-error">{formErrors["occupation"]}</p>
+                    }
+                    <select name="occupation" placeholder='Select an occupation...' style={formSubmitted && formErrors["occupation"] ? {borderColor: "red"} : null} 
+                    onChange={(e) => setOccupation(e.target.value)}>
                     </select>
                 </label>
-                <label> {/* map function ==> option from the GET request */}
-                    State
-                    <select name="password" placeholder='Select a state...' onChange={(e) => setState(e.target.value)}>
-
+                <label> {/* TODO map function ==> option from the GET request */} State 
+                {formSubmitted && <p className = "form-error">{formErrors["state"]}</p>}
+                    <select name="state" placeholder='Select a state...' style={formSubmitted && formErrors["state"] ? {borderColor: "red"} : null} 
+                    onChange={(e) => setState(e.target.value)}>
                     </select>
                 </label>
-                <input type="submit" value="Submit" onSubmit={validationHandler}/>
+                <button type ="button" value="Submit" onClick={validationHandler}>Submit</button>
             </form>
         </div>
     )
-
 }
 
 export default Form;
--- exercise/src/components/Header.js
+++ exercise/src/components/Header.js
@@ -2,7 +2,7 @@ const Header = () => {
     return(
         <div id="Header">
             <h1>Fetch Rewards</h1>
-            <h2>Frontend Take-Home Exercise</h2>
+            <h3>Frontend Take-Home Exercise</h3>
         </div>
     )
 
